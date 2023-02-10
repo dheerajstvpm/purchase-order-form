e1c578b82542b450843f066d833d76e431c263c1
Dheeraj S <dheerajstvpm@gmail.com>
2023-02-10 10:16:42 +0530

    Edit order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PurchaseOrderFormComponent } from './components/purchase-order-form/purchase-order-form.component';
+import { PurchaseOrderListComponent } from './components/purchase-order-list/purchase-order-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "form",
+    pathMatch: "full"
+  },
+  {
+    path: "form",
+    component: PurchaseOrderFormComponent
+  },
+  {
+    path: "list",
+    component: PurchaseOrderListComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
